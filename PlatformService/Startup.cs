db6bddb5e8bbd0df4b7026c68e9ca39f508d65dc
gurjotsandher <gurjotsa143@gmail.com>
2025-03-10 04:51:59 -0700

    Added DB support for K8.

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -13,17 +13,30 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        private readonly IWebHostEnvironment _env;
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options =>
-             options.UseInMemoryDatabase("InMem"));
+            if(_env.IsProduction())
+            {
+                Console.WriteLine("--> Using SQL Server DB");
+                services.AddDbContext<AppDbContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            } else 
+            {
+                Console.WriteLine("--> Using InMem DB");
+                services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("InMem"));
+            }
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
@@ -58,7 +71,7 @@ namespace PlatformService
                 endpoints.MapControllers();
             });
 
-            PrepDb.PrepPopulation(app);
+            PrepDb.PrepPopulation(app, env.IsProduction());
         }
     }
 }
